c9df757e342c3865709db196292972602a7aac52
denismusoev <denis.musoev@yandex.ru>
2024-12-03 20:52:25 +0400

    Update project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import CityInputForm from './components/CityInputForm';
+import MapComponent from './components/MapComponent';
+import { calculateRoute } from './api';
+import { Container } from 'react-bootstrap';
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [route, setRoute] = useState([]);
+  const [totalDistance, setTotalDistance] = useState(0);
+
+  const handleRouteCalculation = async (inputCities, clusters) => {
+    setCities(inputCities);
+
+    const result = await calculateRoute(inputCities, clusters);
+    if (result) {
+      setRoute(result.optimizedRoute.map((_, i, arr) => [arr[i], arr[(i + 1) % arr.length]]));
+      console.log(result.optimizedRoute.map((_, i, arr) => [arr[i], arr[(i + 1) % arr.length]]));
+      setTotalDistance(result.totalDistance);
+    } else {
+      console.error('Не удалось рассчитать маршрут.');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Container>
+        <CityInputForm onSubmit={handleRouteCalculation} />
+        {cities.length > 0 && <h4 className="text-center mt-4">Общая длина маршрута: {totalDistance.toFixed(2)} км</h4>}
+        {cities.length > 0 && <MapComponent cities={cities} route={route} />}
+      </Container>
   );
 }
 
